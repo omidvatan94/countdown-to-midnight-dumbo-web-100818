966ead885c11cb9de95883ba9ecf92a87094a805
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-23 18:54:02 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number = 10
+  while number =< 10 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end
+  puts "HAPPY NEW YEAR!"
 end
